18edf1dee1b3d908e3ce90977956bede4ddeddd0
Pepakayala Veera Ganesh Kumar <pepakayalaveeraganeshkumar@Pepakayalas-MacBook-Air.local>
2023-03-21 13:22:31 +0530

    added day1

---
+++ day1.java
@@ -0,0 +1,21 @@
+class Day {
+    public int numSum(int num){
+        return num+100;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Hello world");
+        Day day = new Day();
+        var result = day.numSum(123);
+        System.out.println(result);
+        
+    }
+}
+
+
+
+
+
+
+
+
